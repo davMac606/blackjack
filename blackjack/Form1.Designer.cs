4c2dc32967111e81c57168ca636021269c2e133f
davMac606 <cl201238@g.unicamp.br>
2024-04-05 17:22:51 -0300

    Adicionar arquivos de projeto.

---
+++ blackjack/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace blackjack
+{
+    partial class Tela
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tela));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.btnStand = new System.Windows.Forms.Button();
+            this.pbCard = new System.Windows.Forms.PictureBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.pbCard2 = new System.Windows.Forms.PictureBox();
+            this.pnlCards = new System.Windows.Forms.Panel();
+            this.pbCard3 = new System.Windows.Forms.PictureBox();
+            this.pbCard4 = new System.Windows.Forms.PictureBox();
+            this.lblScoreE = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCard2)).BeginInit();
+            this.pnlCards.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCard3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCard4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(294, 545);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // btnHit
+            // 
+            this.btnHit.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHit.Location = new System.Drawing.Point(807, 334);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(220, 70);
+            this.btnHit.TabIndex = 2;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            this.btnHit.Click += new System.EventHandler(this.btnHit_Click);
+            // 
+            // btnStand
+            // 
+            this.btnStand.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStand.Location = new System.Drawing.Point(807, 410);
+            this.btnStand.Name = "btnStand";
+            this.btnStand.Size = new System.Drawing.Size(220, 70);
+            this.btnStand.TabIndex = 4;
+            this.btnStand.Text = "Stand";
+            this.btnStand.UseVisualStyleBackColor = true;
+            this.btnStand.Click += new System.EventHandler(this.btnStand_Click);
+            // 
+            // pbCard
+            // 
+            this.pbCard.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbCard.BackgroundImage")));
+            this.pbCard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbCard.Location = new System.Drawing.Point(3, 360);
+            this.pbCard.Name = "pbCard";
+            this.pbCard.Size = new System.Drawing.Size(69, 105);
+            this.pbCard.TabIndex = 5;
+            this.pbCard.TabStop = false;
+            this.pbCard.Click += new System.EventHandler(this.pbCard_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(807, 310);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(52, 21);
+            this.lblScore.TabIndex = 6;
+            this.lblScore.Text = "label2";
+            // 
+            // pbCard2
+            // 
+            this.pbCard2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbCard2.BackgroundImage")));
+            this.pbCard2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbCard2.Location = new System.Drawing.Point(78, 360);
+            this.pbCard2.Name = "pbCard2";
+            this.pbCard2.Size = new System.Drawing.Size(69, 105);
+            this.pbCard2.TabIndex = 7;
+            this.pbCard2.TabStop = false;
+            // 
+            // pnlCards
+            // 
+            this.pnlCards.Controls.Add(this.pbCard4);
+            this.pnlCards.Controls.Add(this.pbCard3);
+            this.pnlCards.Controls.Add(this.pbCard);
+            this.pnlCards.Controls.Add(this.pbCard2);
+            this.pnlCards.Location = new System.Drawing.Point(16, 12);
+            this.pnlCards.Name = "pnlCards";
+            this.pnlCards.Size = new System.Drawing.Size(785, 468);
+            this.pnlCards.TabIndex = 8;
+            // 
+            // pbCard3
+            // 
+            this.pbCard3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbCard3.BackgroundImage")));
+            this.pbCard3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbCard3.Location = new System.Drawing.Point(3, 0);
+            this.pbCard3.Name = "pbCard3";
+            this.pbCard3.Size = new System.Drawing.Size(69, 105);
+            this.pbCard3.TabIndex = 8;
+            this.pbCard3.TabStop = false;
+            // 
+            // pbCard4
+            // 
+            this.pbCard4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbCard4.BackgroundImage")));
+            this.pbCard4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbCard4.Location = new System.Drawing.Point(87, 3);
+            this.pbCard4.Name = "pbCard4";
+            this.pbCard4.Size = new System.Drawing.Size(69, 105);
+            this.pbCard4.TabIndex = 9;
+            this.pbCard4.TabStop = false;
+            // 
+            // lblScoreE
+            // 
+            this.lblScoreE.AutoSize = true;
+            this.lblScoreE.Location = new System.Drawing.Point(807, 12);
+            this.lblScoreE.Name = "lblScoreE";
+            this.lblScoreE.Size = new System.Drawing.Size(52, 21);
+            this.lblScoreE.TabIndex = 9;
+            this.lblScoreE.Text = "label2";
+            // 
+            // Tela
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1039, 492);
+            this.Controls.Add(this.lblScoreE);
+            this.Controls.Add(this.pnlCards);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.btnStand);
+            this.Controls.Add(this.btnHit);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Tela";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Tela_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCard2)).EndInit();
+            this.pnlCards.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCard3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCard4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Button btnStand;
+        private System.Windows.Forms.PictureBox pbCard;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.PictureBox pbCard2;
+        private System.Windows.Forms.Panel pnlCards;
+        private System.Windows.Forms.PictureBox pbCard3;
+        private System.Windows.Forms.PictureBox pbCard4;
+        private System.Windows.Forms.Label lblScoreE;
+    }
+}
+
---
+++ blackjack/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace blackjack
+{
+    public partial class Tela : Form
+    {
+        private void enemyCardDef(int value, PictureBox pictureBox)
+        {
+            switch (value)
+            {
+                case 1:
+                    pictureBox.Image = Image.FromFile(imgpath + "ace of spades.png");
+                    break;
+                case 2:
+                    pictureBox.Image = Image.FromFile(imgpath + "2 of spades.png");
+                    break;
+                case 3:
+                    pictureBox.Image = Image.FromFile(imgpath + "3 of spades.png");
+                    break;
+                case 4:
+                    pictureBox.Image = Image.FromFile(imgpath + "4 of spades.png");
+                    break;
+                case 5:
+                    pictureBox.Image = Image.FromFile(imgpath + "5 of spades.png");
+                    break;
+                case 6:
+                    pictureBox.Image = Image.FromFile(imgpath + "6 of spades.png");
+                    break;
+                case 7:
+                    pictureBox.Image = Image.FromFile(imgpath + "7 of spades.png");
+                    break;
+                case 8:
+                    pictureBox.Image = Image.FromFile(imgpath + "8 of spades.png");
+                    break;
+                case 9:
+                    pictureBox.Image = Image.FromFile(imgpath + "9 of spades.png");
+                    break;
+                case 10:
+                    pictureBox.Image = Image.FromFile(imgpath + "10 of spades.png");
+                    break;
+                case 11:
+                    pictureBox.Image = Image.FromFile(imgpath + "ace of spades.png");
+                    break;
+            }
+        }
+        private void cardDef(int value, PictureBox pictureBox)
+        {
+            switch (value)
+            {
+                case 1:
+                    pictureBox.Image = Image.FromFile(imgpath + "ace of spades.png");
+                    break;
+                case 2:
+                    pictureBox.Image = Image.FromFile(imgpath + "2 of spades.png");
+                    break;
+                case 3:
+                    pictureBox.Image = Image.FromFile(imgpath + "3 of spades.png");
+                    break;
+                case 4:
+                    pictureBox.Image = Image.FromFile(imgpath + "4 of spades.png");
+                    break;
+                case 5:
+                    pictureBox.Image = Image.FromFile(imgpath + "5 of spades.png");
+                    break;
+                case 6:
+                    pictureBox.Image = Image.FromFile(imgpath + "6 of spades.png");
+                    break;
+                case 7:
+                    pictureBox.Image = Image.FromFile(imgpath + "7 of spades.png");
+                    break;
+                case 8:
+                    pictureBox.Image = Image.FromFile(imgpath + "8 of spades.png");
+                    break;
+                case 9:
+                    pictureBox.Image = Image.FromFile(imgpath + "9 of spades.png");
+                    break;
+                case 10:
+                    pictureBox.Image = Image.FromFile(imgpath + "10 of spades.png");
+                    break;
+                case 11:
+                    pictureBox.Image = Image.FromFile(imgpath + "ace of spades.png");
+                    break;
+                    
+            }
+        }
+        int[] cartas = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int score;
+        int scoreEnemy;
+        string imgpath = "C:/Users/User/source/repos/blackjack/blackjack/images/";
+        Random random = new Random();
+        public Tela()
+        {
+            InitializeComponent();
+        }
+
+        private void Tela_Load(object sender, EventArgs e)
+        {
+          
+            int cartaini = cartas[random.Next(0, cartas.Length)];
+            pbCard2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbCard.SizeMode = PictureBoxSizeMode.StretchImage;
+            if ( score > 10 && cartaini == 1)
+            {
+                cartaini = 11;
+            }
+            cardDef(cartaini, pbCard);
+            score = score + cartaini;
+            lblScore.Text = score.ToString();
+            int cartaini2 = cartas[random.Next(0, cartas.Length)];
+            if (score >=10 && cartaini2 == 1)
+            {
+                cartaini2 = 11;
+            }
+            cardDef(cartaini2, pbCard2);
+            score = score + cartaini2;
+            lblScore.Text = score.ToString();
+        }
+        public void hit()
+        {
+            pbCard.SizeMode = PictureBoxSizeMode.StretchImage;
+            int x = pnlCards.Controls.Count * 80;
+            int y = 360;
+            int carta = cartas[random.Next(0, cartas.Length)];
+            if (score > 10 && carta == 1)
+            {
+                carta = 11;
+            }
+            PictureBox pbNewCard = new PictureBox();
+            pbNewCard.Location = new Point(x, y);
+            pbNewCard.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbNewCard.Width = 69;
+            pbNewCard.Height = 105;
+
+            cardDef(carta, pbNewCard);
+            score = score + carta;
+            lblScore.Text = "Pontuação:" + score.ToString();
+            pnlCards.Controls.Add(pbNewCard);
+            
+            if (score > 21)               
+            {
+                MessageBox.Show("Você perdeu!");
+                Application.Exit();
+            }
+        }
+        public int enemyStartingCards()
+        {
+            int cartaE = cartas[random.Next(0, cartas.Length)];
+            pbCard3.SizeMode = PictureBoxSizeMode.StretchImage;
+            if (score > 10 && cartaE == 1)
+            {
+                cartaE = 11;
+            }
+            enemyCardDef(cartaE, pbCard3);
+            scoreEnemy += cartaE;
+            int cartaE2 = cartas[random.Next(0, cartas.Length)];
+            pbCard4.SizeMode = PictureBoxSizeMode.StretchImage;
+            if (score > 10 && cartaE2 == 1)
+            {
+                cartaE2 = 11;
+            }
+            enemyCardDef(cartaE2, pbCard4);
+            scoreEnemy += cartaE2;
+            return scoreEnemy;
+        }
+        public void enemyturn()
+        {
+            enemyStartingCards();
+            int x = pnlCards.Controls.Count * 80;
+            int y = 0;
+            int cartaE3 = cartas[random.Next(0, cartas.Length)];
+            if (score > 10 && cartaE3 == 1)
+            {
+                cartaE3 = 11;
+            }
+            PictureBox pbNewCardE = new PictureBox();
+            pbNewCardE.Location = new Point(x, y);
+            pbNewCardE.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbNewCardE.Width = 69;
+            pbNewCardE.Height = 105;
+
+            
+            enemyCardDef(cartaE3, pbNewCardE);
+            scoreEnemy += cartaE3;
+            lblScoreE.Text = "Pontuação:" + scoreEnemy.ToString();
+
+        }
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            hit();
+        }
+
+        private void btnStand_Click(object sender, EventArgs e)
+        {
+            enemyturn();
+            while (scoreEnemy < 21)
+            {
+                int choice = random.Next(0, 2);
+                if (choice == 1)
+                {
+                    enemyturn();
+                }
+            }
+            if (scoreEnemy > score && scoreEnemy <= 21)
+            {
+                MessageBox.Show("Você perdeu!");
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("Você ganhou!");
+                Application.Exit();
+            }   
+            
+        }
+
+        private void pbCard_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
